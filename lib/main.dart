f48496cc70a1dbe15da07e01279c8da976d4099f
Joshua Chen <jchen1122@berkeley.edu>
2020-01-14 22:02:00 -0800

    Create intray top bar and button #2

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'models/global.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Prod App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Prod app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return MaterialApp(
+      color: Colors.yellow,
+      home: SafeArea(
+              child: DefaultTabController(
+          length: 3,
+          child: new Scaffold(
+            body: Stack(
+                children: <Widget>[
+                  
+                  TabBarView(
+                children: [
+                  new Container(
+                    color: darkGreyColor,
+                  ),
+                  new Container(color: Colors.orange,),
+                  new Container(
+                    color: Colors.lightGreen,
+                  ),
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 50),
+                height: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50)
+                  ),
+                  color: Colors.white,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Intray", style: intrayTitleStyle),
+                    Container()
+                  ],
+                ),
+                ),
+                Container(
+                  height: 70,
+                  width: 70,
+                  margin: EdgeInsets.only(top:115, left: MediaQuery.of(context).size.width * 0.5 - 30),
+                  child: FloatingActionButton(
+                    child: Icon(Icons.add, size: 60),
+                    backgroundColor: redColor,
+                    onPressed: () {},
+                  ),
+                )
+            ]
+            ),
+            appBar: AppBar(
+              elevation: 0,
+                title: new TabBar(
+                tabs: [
+                  Tab(
+                    icon: new Icon(Icons.home),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.rss_feed),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.perm_identity),
+                  )
+                ],
+                labelColor: darkGreyColor,
+                unselectedLabelColor: Colors.blue,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorPadding: EdgeInsets.all(5.0),
+                indicatorColor: Colors.transparent,
+              ),
+              backgroundColor: Colors.white,
+            ),
+            backgroundColor: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/global.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF212128);
+Color redColor = new Color(0xFFDC4F64);
+
+TextStyle intrayTitleStyle = new TextStyle(fontFamily: 'Times New Roman', color: darkGreyColor, fontSize: 40);
